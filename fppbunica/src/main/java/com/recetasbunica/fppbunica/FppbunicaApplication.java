f2fb0c858d7932109477816548cc283c43f17b38
Ruben Mrblue <sanchez.alvarez.ruben@gmail.com>
2025-03-12 18:48:23 +0100

    set up project

---
+++ fppbunica/src/main/java/com/recetasbunica/fppbunica/FppbunicaApplication.java
@@ -0,0 +1,13 @@
+package com.recetasbunica.fppbunica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FppbunicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FppbunicaApplication.class, args);
+	}
+
+}
